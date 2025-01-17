8bca498785f15d7ef1a5a84c564d41f90403a6d0
makarychevDN <39443709+makarychevDN@users.noreply.github.com>
2025-01-17 22:05:40 +0500

    now cards have animations

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -1,6 +1,18 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class Card : MonoBehaviour
+public class Card : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     [SerializeField] private int cost;
+    [SerializeField] private Animator animator;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        animator.SetBool("Hovered", true);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        animator.SetBool("Hovered", false);
+    }
 }
