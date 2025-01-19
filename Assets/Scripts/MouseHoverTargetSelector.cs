f93f57c3f998a3a802ddcf8a86a1df123b93bd2d
makarychevDN <39443709+makarychevDN@users.noreply.github.com>
2025-01-19 23:58:56 +0500

    a card which is able to attack certain target has been added

---
+++ Assets/Scripts/MouseHoverTargetSelector.cs
@@ -0,0 +1,40 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseHoverTargetSelector : TargetsForCardSelector
+{
+    [SerializeField] private bool enemiesIncluded = true;
+    [SerializeField] private bool alliesIncluded;
+
+    public override List<Character> SelectTargets()
+    {
+        return new List<Character> { TryToGetCharacterByRaycast() };
+    }
+
+    private Character TryToGetCharacterByRaycast()
+    {
+        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        RaycastHit2D hit = Physics2D.Raycast(mousePosition, Vector2.zero);
+
+        if (hit.collider != null)
+        {
+            var character = hit.collider.gameObject.GetComponent<Character>();
+            return FilterSelectedTarget(character);
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    private Character FilterSelectedTarget(Character target)
+    {
+        if (!alliesIncluded && playerInput.CharactersList.Contains(target))
+            return null;
+
+        if (!enemiesIncluded && playerInput.EnemyTeam.CharactersList.Contains(target))
+            return null;
+
+        return target;
+    }
+}
