7e36f30597bed90c7efe4bb119c0d4603a7a44b6
makarychevDN <39443709+makarychevDN@users.noreply.github.com>
2025-01-20 20:18:03 +0500

    Punching Dummy AI is able to display intentions now

--- Assets/Scripts/Inputs/PunchingDummyAI.cs
+++ Assets/Scripts/Inputs/PunchingDummyAI.cs
@@ -1,16 +1,43 @@
+using System;
 using System.Collections.Generic;
+using UnityEngine;
 
 public class PunchingDummyAI : BaseInput
 {
+    private Dictionary<Character, BaseBattleEffect> preparedBattleEffects = new();
+
+    public override void Init(BaseInput enemyTeam)
+    {
+        base.Init(enemyTeam);
+        PrepareIntentions();
+    }
+
     public async override void StartTurn()
     {
         base.StartTurn();
 
         foreach (var character in charactersList)
         {
-            await character.AvailableBattleEffects.GetRandomElement().UseEffectOnTargets(new List<Character>() { character });
+            await preparedBattleEffects[character].UseEffectOnTargets(new List<Character>() { character });
         }
 
         EndTurn();
+    }
+
+    private void PrepareIntentions()
+    {
+        foreach (var character in charactersList)
+        {
+            if (preparedBattleEffects.ContainsKey(character))
+            {
+                preparedBattleEffects[character] = character.AvailableBattleEffects.GetRandomElement();
+            }
+            else
+            {
+                preparedBattleEffects.Add(character, character.AvailableBattleEffects.GetRandomElement());
+            }
+
+            character.DisplayIntention(preparedBattleEffects[character]);
+        }
     }
 }
