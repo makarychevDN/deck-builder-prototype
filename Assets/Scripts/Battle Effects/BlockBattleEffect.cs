b3c864c15569a00b256bb450469f850c67077d16
makarychevDN <39443709+makarychevDN@users.noreply.github.com>
2025-01-20 16:26:38 +0500

    a block card has been added

---
+++ Assets/Scripts/Battle Effects/BlockBattleEffect.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class BlockBattleEffect : BaseBattleEffect
+{
+    [SerializeField] private int blockValue;
+
+    public override Task UseEffectOnTarget(Character target) => UseEffectOnTargets(new List<Character> { target });
+
+    public override async Task UseEffectOnTargets(List<Character> targets)
+    {
+        OnEffectWithAnimationTypeUsed.Invoke(animationType);
+        await Task.Delay(timeBeforeImpact);
+        targets.ForEach(target => target.TakeBlock(blockValue));
+        await Task.Delay(timeForWholeProcess - timeBeforeImpact);
+    }
+}
